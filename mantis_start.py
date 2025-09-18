2a3030c03766c35b6a447154484abf00d4f847d5
Nghia2123 <23521018@gm.uit.edu.vn>
2025-09-18 15:16:34 +0700

    restore original mantis

--- mantis_start.py
+++ mantis_start.py
@@ -2,7 +2,6 @@ import sys
 import time
 import argparse
 import importlib
-import os
 
 from Mantis.InjectionManager.default import DefaultInjectionManager
 from Mantis.utils import get_local_ip, get_public_ip
@@ -10,38 +9,9 @@ from Mantis.utils.logger import logger
 from Mantis.InjectionManager.interface import InterfaceClientRPC
 from Mantis.utils.Paper import forward_proxy
 
-def scan_config_options():
-    config_dir = "confs"
-    config_options = []
-    for filename in os.listdir(config_dir):
-        if filename.endswith(".py") and not filename.startswith("__"):
-            module_name = f"{config_dir}.{filename[:-3]}"  # remove .py
-            config_options.append(module_name)
-    return config_options
 
-
-def select_config():
-	config_options = scan_config_options()
-	if not config_options:
-		print("No configuration files found in 'confs' directory.")
-		sys.exit(1)
-
-	print("Select configuration:")
-	for idx, option in enumerate(config_options, 1):
-		print(f"{idx}. {option}")
-	while True:
-		try:
-			choice = int(input("Enter your choice (number): "))
-			if 1 <= choice <= len(config_options):
-				return config_options[choice - 1]
-			else:
-				print("Invalid choice. Try again.")
-		except ValueError:
-			print("Invalid input. Enter a number.")
-
-
-def main(args, conf_file, with_forward=False):
-	conf = importlib.import_module(conf_file)
+def main(args, with_forward=False):
+	conf = importlib.import_module(args.conf_file)
 	
 	local_ip = get_local_ip()
 	public_ip = get_public_ip()
@@ -81,18 +51,16 @@ def main(args, conf_file, with_forward=False):
 
 
 if __name__ == '__main__':
-	print("""
-	This script simulates deploying Mantis on a remote machine.
-	- Select the defense configuration.
-	""")
 
 	parser = argparse.ArgumentParser(description='Run Mantis on the host machine based on the configuration file provided')
 
+	parser.add_argument('conf_file', type=str,
+						help='Path to the configuration file (e.g., confs.ftp_hackback_rshell)')
 	parser.add_argument(
 		'--run_rpc_interface',
 		action='store_true',
 		help="Run RPC server for external clients"
 	)
+	
 	args = parser.parse_args()
-	conf_file = select_config()
-	main(args, conf_file, with_forward=False)
+	main(args, with_forward=False)
